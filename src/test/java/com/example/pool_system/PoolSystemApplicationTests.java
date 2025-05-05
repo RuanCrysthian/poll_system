9d2c513eccf77fce0642b6adf1bc52415d0fee69
Ruan Ferraz <ruanlima155@gmail.com>
2025-05-05 16:37:00 -0300

    build: create setup

---
+++ src/test/java/com/example/pool_system/PoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pool_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
